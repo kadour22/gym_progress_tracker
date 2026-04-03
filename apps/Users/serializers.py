4f82619fb2d5a11516bc457dc3735e9e4bd081f1
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-04-03 06:29:42 +0100

    restart project with service layer arctectuer

--- apps/Users/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Promgram
-
-class ProgramSerializer(serializers.ModelSerializer) :
-    class Meta :
-        model  = Promgram
-        fields = "__all__"
--- apps/Users/views.py
+++ apps/Users/views.py
@@ -1,15 +1,9 @@
-from .serializers import ProgramSerializer
-from .models import Promgram
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-# class my_view(APIView) :
+class test_view(APIView) :
 
-#     def post(self, request) :
-
-#         serializer = ProgramSerializer(data=request.data)
-#         if serializer.is_valid() :
-
-#             generate_program = serializer.save()
+    def get(self, request) :
+        return Response("hello")
