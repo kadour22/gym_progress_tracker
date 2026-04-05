2134b8ad75667cfa96ca784eafaccc82b0fe98ef
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-04-05 12:09:56 +0100

    add permissions

---
+++ apps/Program/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Program, WorkoutDayLog
+
+
+admin.site.register(Program)
+admin.site.register(WorkoutDayLog)
--- apps/Program/views.py
+++ apps/Program/views.py
@@ -3,8 +3,10 @@ from .services.crud.create import ProgramService
 # rest imports
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import permissions
 
 class GenerateProgramView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
     def post(self, request):
         program_service = ProgramService()
         result = program_service.create_program_info(data=request.data,user=request.user)
