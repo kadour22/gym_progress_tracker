0839e71e1d763212d4a7ea7258fc4610d5dee49b
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-04-06 14:31:48 +0100

    create DeleteProgramView url

--- apps/Program/urls.py
+++ apps/Program/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("ai-generate-program/", views.GenerateProgramView.as_view()),
+    path("program/<int:program_id>/", views.DeleteProgramView.as_view()),
 ]
