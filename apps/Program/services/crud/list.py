06cf9607a96b86a62322d2da08912f308c08484d
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-04-06 21:21:29 +0100

    add get_program_by_id to ProgramDataService

--- apps/Program/services/crud/list.py
+++ apps/Program/services/crud/list.py
@@ -1,6 +1,6 @@
 from ...serializers import ProgramDataSerializer, ProgramDataSerializer
 from ...models import Program, ProgramData
-
+from django.shortcuts import get_object_or_404
 class ProgramDataService:
 
     def list_all_program_data(self,user) :
@@ -11,3 +11,8 @@ class ProgramDataService:
         return ({
             "data":serializer.data,
         })
+    
+    def get_program_by_id(self,prog_id) :
+        program = get_object_or_404(ProgramData, id = prog_id)
+        serializer = ProgramDataSerializer(program,many=False)
+        return ({ "data":serializer.data  })
