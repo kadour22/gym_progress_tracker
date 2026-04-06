9ac6550978490f65a554f86bb768896372fcd2de
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-04-06 21:22:10 +0100

    udapte ProgramDataService

--- apps/Program/services/crud/list.py
+++ apps/Program/services/crud/list.py
@@ -1,16 +1,14 @@
 from ...serializers import ProgramDataSerializer, ProgramDataSerializer
 from ...models import Program, ProgramData
 from django.shortcuts import get_object_or_404
+
+
 class ProgramDataService:
 
     def list_all_program_data(self,user) :
-
         prog_data = ProgramData.objects.filter(program__user = user).order_by('-createdAt')
         serializer = ProgramDataSerializer(prog_data,many=True)
-
-        return ({
-            "data":serializer.data,
-        })
+        return ({ "data":serializer.data})
     
     def get_program_by_id(self,prog_id) :
         program = get_object_or_404(ProgramData, id = prog_id)
