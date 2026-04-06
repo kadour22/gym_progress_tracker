6bf18185811e711a15e629fa6b0446cf79c9e181
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-04-06 21:34:25 +0100

    create ProgramDataView routes

--- apps/Program/urls.py
+++ apps/Program/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path("ai-generate-program/", views.GenerateProgramView.as_view()),
     path("program/<int:program_id>/", views.DeleteProgramView.as_view()),
+
+    path("program-data/", views.ProgramDataView.as_view()),
+    path("program-data/program/<int:prog_id>/", views.ProgramDataView.as_view()),
+    
 ]
