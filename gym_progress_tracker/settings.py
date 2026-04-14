3277642c9308b244c1717ce2d712bd0f6bc57e39
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-04-14 12:42:44 +0100

    configuer staticfiles

--- gym_progress_tracker/settings.py
+++ gym_progress_tracker/settings.py
@@ -81,15 +81,22 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  
+]
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"  
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 OPENAI_KEY = os.getenv("OPENAI_KEY")
 OPENAI_AI_URL = os.getenv("OPENAI_AI_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
 print(SECRET_KEY)
-DEBUG = False
+DEBUG = True
 CORS_ALLOWED_ORIGINS = [
     origin.strip()
     for origin in os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
