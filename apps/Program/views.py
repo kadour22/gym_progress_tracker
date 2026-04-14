0605cdb31173e5d251c84b75b9615c1139603b8a
Abdelkader <zaroui.abedlkader777@gmail.com>
2026-04-14 14:13:51 +0100

    add DashboardView

--- apps/Program/views.py
+++ apps/Program/views.py
@@ -10,7 +10,9 @@ from django.db.transaction import atomic
 from .permissions.permissions import IsProgramOwner
 from .serializers import ProgramDataSerializer,ProgramSerializer
 from rest_framework.response import Response
-
+from .utils.compute_consistency import calculate_consistency
+from .utils.program_progress import calculate_data_activity,calculate_program_progress
+from .models import Program,ProgramData
 class GenerateProgramView(APIView):
     permission_classes = [permissions.IsAuthenticated]
     def post(self, request):
@@ -48,3 +50,28 @@ class SingleProgramData(APIView):
         self.check_object_permissions(request, program)
         serializer = ProgramDataSerializer(program)
         return Response(serializer.data)
+
+class DashboardView(APIView):
+    def get(self, request):
+        user = request.user
+
+        program = Program.objects.filter(user=user).first()
+
+        if not program:
+            return Response({
+                "programs": 0,
+                "programs_data": 0,
+                "consistency": 0,
+                "progress": 0,
+                "activity": 0
+            })
+
+        data = {
+            "programs": Program.objects.filter(user=user).count(),
+            "programs_data": ProgramData.objects.filter(user=user).count(),
+            "consistency": calculate_consistency(program),
+            "progress": calculate_program_progress(program),
+            "activity": calculate_data_activity(program),
+        }
+
+        return Response(data)
